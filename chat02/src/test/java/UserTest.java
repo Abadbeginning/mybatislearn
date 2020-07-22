80ff723917f1648dc21f7b5d2a6a70e9b68e5fca
e <2029618609@qq.com>
2020-07-22 10:45:35 +0800

    Java干货铺子 MyBatis系列第3篇：Mybatis使用详解（1） Mapper接口的使用

--- chat02/src/test/java/UserTest.java
+++ chat02/src/test/java/UserTest.java
@@ -1,3 +1,4 @@
+import com.google.UserMapper;
 import com.google.UserModel;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.io.Resources;
@@ -46,6 +47,32 @@ public class UserTest {
             // 插入
             int result = sqlSession.insert("com.google.UserMapper.insertUser", userModel);
 
+            log.info("插入影响行数：{}", result);
+            // openSession(true)时 无需手动提交事务
+//            sqlSession.commit();
+
+        } finally {
+            //关闭SqlSession
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void insertUser1(){
+        // 自动提交
+        SqlSession sqlSession = this.sqlSessionFactory.openSession(true);
+        // 非自动提交
+//        SqlSession sqlSession = this.sqlSessionFactory.openSession();
+        try {
+            //执行业务操作，如：增删改查
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+            // 创建usermodel对象
+            UserModel userModel = UserModel.builder().id(1L).age(123).name("我的第三方").salary(123.3).sex(0).build();
+
+            // 插入
+//            int result = sqlSession.insert("com.google.UserMapper.insertUser", userModel);
+            int result = mapper.insertUser(userModel);
             log.info("插入影响行数：{}", result);
             // openSession(true)时 无需手动提交事务
 //            sqlSession.commit();
